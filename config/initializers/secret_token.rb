c13d0f5297416826b518ce9875bff263c308da29
Henry McBride <henry.mcbride@gmail.com>
2011-07-15 19:47:17 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OT3::Application.config.secret_token = '31be9fb8599432dd2882e41d567ebfa6b108711e87ba28044f0a31f9e015d9fd4323c690180bafc5104cb5afe50f569cb731feef9d76cfd1b4ba8f7fcc08ae14'
